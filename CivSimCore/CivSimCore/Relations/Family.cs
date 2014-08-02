acc49d9cd717249ba60bd009bcb181ff3a13a03c
renezaal <renezaal@gmail.com>
2014-08-02 14:53:53 +0200

    worked out the idea of a pool a bit made some basic classes

--- CivSimCore/CivSimCore/Relations/Family.cs
+++ CivSimCore/CivSimCore/Relations/Family.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CivSimCore
+namespace CivSimCore.Relations
 {
-    public class Class1
+    class Family
     {
     }
 }
